276b638bdb97bf2cb77060716c160cedb9d11f16
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-10-05 18:40:13 +0600

    :art: start second part

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const getEl = el => document.querySelector(el);
+
+const galleryContainer = getEl('.gallery');
+
+const galleryMarkup = galleryItems.map(({original, description, preview}) => {
+  return `
+              <a class="gallery__item" href="${original}">
+                  <img class="gallery__image" src="${preview}" alt="${description}">
+              </a>`;
+}).join('');
+
+galleryContainer.innerHTML = galleryMarkup;
+
+function openHandler(event) {
+  event.preventDefault();
+  if (event.target !== event.currentTarget) {
+    const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+  }
+}
+
+galleryContainer.addEventListener('click', openHandler);
+
 console.log(galleryItems);
