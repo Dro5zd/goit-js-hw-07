897e084db621d5e863711aaa589a009fffd029c8
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-10-06 09:37:25 +0600

    :rocket: task completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from './gallery-items.js';
 // Change code below this line
 
 const getEl = el => document.querySelector(el);
@@ -6,10 +6,9 @@ const getEl = el => document.querySelector(el);
 const galleryContainer = getEl('.gallery');
 
 const galleryMarkup = galleryItems.map(({original, description, preview}) => {
-  return `
-              <a class="gallery__item" href="${original}">
-                  <img class="gallery__image" src="${preview}" alt="${description}">
-              </a>`;
+  return `<a class="gallery__item" href="${original}">
+              <img class="gallery__image" src="${preview}" alt="${description}">
+           </a>`;
 }).join('');
 
 galleryContainer.innerHTML = galleryMarkup;
@@ -17,10 +16,12 @@ galleryContainer.innerHTML = galleryMarkup;
 function openHandler(event) {
   event.preventDefault();
   if (event.target !== event.currentTarget) {
-    const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250
+    });
   }
 }
 
 galleryContainer.addEventListener('click', openHandler);
 
-console.log(galleryItems);
